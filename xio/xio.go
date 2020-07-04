1f676a4c6b913b9eeba8075e22486a7f510ba580
Centny <centny@gmail.com>
2020-07-04 21:08:21 +0800

    add Copy for Package.

--- xio/xio.go
+++ xio/xio.go
@@ -2,7 +2,10 @@ package xio
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"net"
+	"reflect"
 )
 
 //WriteJSON will marshal value to string and write to io.Writer
@@ -10,6 +13,48 @@ func WriteJSON(w io.Writer, v interface{}) (n int, err error) {
 	data, err := json.Marshal(v)
 	if err == nil {
 		n, err = w.Write(data)
+	}
+	return
+}
+
+func CopyPacketConn(dst interface{}, src net.PacketConn) (l int64, err error) {
+	buffer := make([]byte, 2*1024)
+	for {
+		n, from, xerr := src.ReadFrom(buffer)
+		if xerr != nil {
+			err = xerr
+			break
+		}
+		if out, ok := dst.(net.PacketConn); ok {
+			n, xerr = out.WriteTo(buffer[0:n], from)
+		} else if out, ok := dst.(io.Writer); ok {
+			n, xerr = out.Write(buffer[0:n])
+		} else {
+			xerr = fmt.Errorf("not supported dst by type %v", reflect.TypeOf(dst))
+		}
+		if xerr != nil {
+			err = xerr
+			break
+		}
+		l += int64(n)
+	}
+	return
+}
+
+func CopyPacketTo(dst net.PacketConn, to net.Addr, src io.Reader) (l int64, err error) {
+	buffer := make([]byte, 2*1024)
+	for {
+		n, xerr := src.Read(buffer)
+		if xerr != nil {
+			err = xerr
+			break
+		}
+		n, xerr = dst.WriteTo(buffer[0:n], to)
+		if xerr != nil {
+			err = xerr
+			break
+		}
+		l += int64(n)
 	}
 	return
 }
--- xio/xio_test.go
+++ xio/xio_test.go
@@ -1,10 +1,131 @@
 package xio
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
+	"net"
 	"testing"
+	"time"
 )
 
 func TestSimple(t *testing.T) {
 	WriteJSON(ioutil.Discard, map[string]interface{}{})
+}
+
+func TestCopyPacketConn(t *testing.T) {
+	packet1, _ := net.ListenPacket("udp", ":12332")
+	go CopyPacketConn(packet1, packet1)
+	raw, err := net.Dial("udp", "127.0.0.1:12332")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer raw.Close()
+	udp := raw.(*net.UDPConn)
+	_, err = udp.Write([]byte("123"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	buffer := make([]byte, 123)
+	n, err := udp.Read(buffer)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if string(buffer[0:n]) != "123" {
+		t.Error("error")
+		return
+	}
+	packet1.Close()
+	time.Sleep(100 * time.Millisecond)
+}
+
+func TestCopyPacketConnToWriter(t *testing.T) {
+	packet1, _ := net.ListenPacket("udp", ":12332")
+	buffer := bytes.NewBuffer(nil)
+	go CopyPacketConn(buffer, packet1)
+	raw, err := net.Dial("udp", "127.0.0.1:12332")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer raw.Close()
+	udp := raw.(*net.UDPConn)
+	_, err = udp.Write([]byte("123"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	time.Sleep(100 * time.Millisecond)
+	if string(buffer.Bytes()) != "123" {
+		t.Error("error")
+		return
+	}
+	packet1.Close()
+	time.Sleep(100 * time.Millisecond)
+}
+
+func TestCopyPacketConnErrorNotSupported(t *testing.T) {
+	packet1, _ := net.ListenPacket("udp", ":12332")
+	raw, err := net.Dial("udp", "127.0.0.1:12332")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer raw.Close()
+	udp := raw.(*net.UDPConn)
+	_, err = udp.Write([]byte("123"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = CopyPacketConn(t, packet1)
+	if err == nil {
+		t.Error(nil)
+	}
+	packet1.Close()
+	time.Sleep(100 * time.Millisecond)
+}
+
+func TestCopyPacketTo(t *testing.T) {
+	packet1, _ := net.ListenPacket("udp", ":12332")
+	raw, err := net.Dial("udp", "127.0.0.1:12332")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer raw.Close()
+
+	//
+	udp := raw.(*net.UDPConn)
+	_, err = CopyPacketTo(packet1, udp.LocalAddr(), bytes.NewBuffer([]byte("123")))
+	if err != io.EOF {
+		t.Error(err)
+		return
+	}
+	buffer := make([]byte, 1024)
+	n, err := udp.Read(buffer)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	time.Sleep(100 * time.Millisecond)
+	if string(buffer[0:n]) != "123" {
+		t.Error("error")
+		return
+	}
+	packet1.Close()
+	time.Sleep(100 * time.Millisecond)
+}
+
+func TestCopyPacketToError(t *testing.T) {
+	packet1, _ := net.ListenPacket("udp", ":12332")
+	packet1.Close()
+	_, err := CopyPacketTo(packet1, packet1.LocalAddr(), bytes.NewBuffer([]byte("123")))
+	if err == nil {
+		t.Error(err)
+		return
+	}
 }
