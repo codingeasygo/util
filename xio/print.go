fed46489882877843455d1a06bed672c5e46866d
Centny <centny@gmail.com>
2020-09-30 22:17:05 +0800

    add print conn

---
+++ xio/print.go
@@ -0,0 +1,107 @@
+package xio
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"time"
+)
+
+//PrintConn is net.Conn to print the transfter data
+type PrintConn struct {
+	Base io.ReadWriteCloser
+	Name string
+	Mode int
+}
+
+//NewPrintConn will create new PrintConn
+func NewPrintConn(name string, base io.ReadWriteCloser) (conn *PrintConn) {
+	conn = &PrintConn{Base: base, Name: name}
+	return
+}
+
+func (p *PrintConn) Read(b []byte) (n int, err error) {
+	n, err = p.Base.Read(b)
+	if err == nil {
+		switch p.Mode {
+		case 0x10:
+			fmt.Printf("%v Read %v bytes %v\n", p.Name, n, string(b[:n]))
+		default:
+			fmt.Printf("%v Read %v bytes % 02x\n", p.Name, n, b[:n])
+		}
+	} else {
+		fmt.Printf("%v Read error %v\n", p.Name, err)
+	}
+	return
+}
+
+func (p *PrintConn) Write(b []byte) (n int, err error) {
+	n, err = p.Base.Write(b)
+	if err == nil {
+		switch p.Mode {
+		case 0x10:
+			fmt.Printf("%v Write %v bytes %v\n", p.Name, n, string(b[:n]))
+		default:
+			fmt.Printf("%v Write %v bytes % 02x\n", p.Name, n, b[:n])
+		}
+	} else {
+		fmt.Printf("%v Write error %v\n", p.Name, err)
+	}
+	return
+}
+
+// LocalAddr returns the local network address.
+func (p *PrintConn) LocalAddr() net.Addr {
+	if conn, ok := p.Base.(net.Conn); ok {
+		return conn.LocalAddr()
+	}
+	return p
+}
+
+// RemoteAddr returns the remote network address.
+func (p *PrintConn) RemoteAddr() net.Addr {
+	if conn, ok := p.Base.(net.Conn); ok {
+		return conn.RemoteAddr()
+	}
+	return p
+}
+
+// SetDeadline for net.Conn
+func (p *PrintConn) SetDeadline(t time.Time) error {
+	if conn, ok := p.Base.(net.Conn); ok {
+		return conn.SetDeadline(t)
+	}
+	return nil
+}
+
+// SetReadDeadline for net.Conn
+func (p *PrintConn) SetReadDeadline(t time.Time) error {
+	if conn, ok := p.Base.(net.Conn); ok {
+		return conn.SetReadDeadline(t)
+	}
+	return nil
+}
+
+// SetWriteDeadline for net.Conn
+func (p *PrintConn) SetWriteDeadline(t time.Time) error {
+	if conn, ok := p.Base.(net.Conn); ok {
+		return conn.SetWriteDeadline(t)
+	}
+	return nil
+}
+
+//Network impl net.Addr
+func (p *PrintConn) Network() string {
+	return "print"
+}
+
+func (p *PrintConn) String() string {
+	return fmt.Sprintf("%v", p.Name)
+}
+
+//Close will close base
+func (p *PrintConn) Close() (err error) {
+	err = p.Base.Close()
+	fmt.Printf("%v Close %v\n", p.Name, err)
+	return
+}
---
+++ xio/print_test.go
@@ -0,0 +1,107 @@
+package xio
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"testing"
+	"time"
+)
+
+type printConnTest struct {
+	data chan []byte
+	err  error
+}
+
+func (p *printConnTest) Read(b []byte) (n int, err error) {
+	if p.err == nil {
+		data := <-p.data
+		n = copy(b, data)
+	}
+	err = p.err
+	return
+}
+
+func (p *printConnTest) Write(b []byte) (n int, err error) {
+	if p.err == nil {
+		p.data <- b
+		n = len(b)
+	}
+	err = p.err
+	return
+}
+
+// LocalAddr returns the local network address.
+func (p *printConnTest) LocalAddr() net.Addr {
+	return p
+}
+
+// RemoteAddr returns the remote network address.
+func (p *printConnTest) RemoteAddr() net.Addr {
+	return p
+}
+
+// SetDeadline for net.Conn
+func (p *printConnTest) SetDeadline(t time.Time) error {
+	return nil
+}
+
+// SetReadDeadline for net.Conn
+func (p *printConnTest) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+// SetWriteDeadline for net.Conn
+func (p *printConnTest) SetWriteDeadline(t time.Time) error {
+	return nil
+}
+
+//Network impl net.Addr
+func (p *printConnTest) Network() string {
+	return "print"
+}
+
+func (p *printConnTest) String() string {
+	return "test"
+}
+
+func (p *printConnTest) Close() (err error) {
+	return
+}
+
+func TestPrint(t *testing.T) {
+	test := &printConnTest{
+		data: make(chan []byte, 1024),
+	}
+	print := NewPrintConn("testing", test)
+	//
+	print.Mode = 0
+	print.Write([]byte("abc"))
+	print.Read(make([]byte, 1024))
+	//
+	print.Mode = 0x10
+	print.Write([]byte("abc"))
+	print.Read(make([]byte, 1024))
+	//
+	test.err = fmt.Errorf("closed")
+	print.Write([]byte("abc"))
+	print.Read(make([]byte, 1024))
+	//
+	print.Close()
+	//
+	print.SetDeadline(time.Now())
+	print.SetReadDeadline(time.Now())
+	print.SetWriteDeadline(time.Now())
+	print.LocalAddr()
+	print.RemoteAddr()
+	print.Network()
+	fmt.Println(print.String())
+	//
+	test1 := bytes.NewBuffer(nil)
+	print1 := NewPrintConn("testing", NewCombinedReadWriteCloser(test1, test1, nil))
+	print1.SetDeadline(time.Now())
+	print1.SetReadDeadline(time.Now())
+	print1.SetWriteDeadline(time.Now())
+	print1.LocalAddr()
+	print1.RemoteAddr()
+}
