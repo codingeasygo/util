8c6f9374af288d660070a82d0a530aaa3f2dcecf
centny <centny@gmail.com>
2022-06-23 12:36:09 +0800

    add attr scan.

---
+++ attrscan/attrscan.go
@@ -0,0 +1,106 @@
+package attrscan
+
+import (
+	"reflect"
+	"strings"
+)
+
+type NilChecker interface {
+	IsNil() bool
+}
+
+type ZeroChecker interface {
+	IsZero() bool
+}
+
+type NameConv func(on, name string, field reflect.StructField) string
+
+type Scanner struct {
+	Tag      string
+	NameConv NameConv
+}
+
+var Default = &Scanner{
+	Tag: "json",
+	NameConv: func(on, name string, field reflect.StructField) string {
+		return name
+	},
+}
+
+func (s *Scanner) checkValue(val reflect.Value, incNil, incZero bool) bool {
+	if !val.IsValid() {
+		return incNil
+	}
+	v := val.Interface()
+	if c, ok := v.(NilChecker); ok {
+		return !c.IsNil() || incNil
+	}
+	if c, ok := v.(ZeroChecker); ok {
+		return !c.IsZero() || incZero
+	}
+	kind := val.Kind()
+	if kind == reflect.Ptr && val.IsNil() && !incNil {
+		return false
+	}
+	if kind == reflect.Ptr && !val.IsNil() {
+		val = reflect.Indirect(val)
+	}
+	if (!val.IsValid() || val.IsZero()) && !incZero {
+		return false
+	}
+	return true
+}
+
+func FilterFieldCall(on string, v interface{}, filter string, call func(fieldName, fieldFunc string, field reflect.StructField, value interface{})) {
+	Default.FilterFieldCall(on, v, filter, call)
+}
+
+func (s *Scanner) FilterFieldCall(on string, v interface{}, filter string, call func(fieldName, fieldFunc string, field reflect.StructField, value interface{})) {
+	reflectValue := reflect.Indirect(reflect.ValueOf(v))
+	reflectType := reflectValue.Type()
+	var fieldAll = map[string]string{}
+	var isExc = false
+	var incNil, incZero bool
+	if len(filter) > 0 {
+		filter = strings.TrimSpace(filter)
+		parts := strings.SplitN(filter, "#", 2)
+		isExc = strings.HasPrefix(parts[0], "^")
+		if len(parts[0]) > 0 {
+			for _, fieldItem := range strings.Split(strings.TrimPrefix(parts[0], "^"), ",") {
+				fieldParts := strings.SplitN(strings.Trim(strings.TrimSpace(fieldItem), ")"), "(", 2)
+				if len(fieldParts) > 1 {
+					fieldAll[fieldParts[1]] = fieldParts[0]
+				} else {
+					fieldAll[fieldParts[0]] = ""
+				}
+			}
+		}
+		if len(parts) > 1 && len(parts[1]) > 0 {
+			incNil = strings.Contains(","+parts[1]+",", ",nil,") || strings.Contains(","+parts[1]+",", ",all,")
+			incZero = strings.Contains(","+parts[1]+",", ",zero,") || strings.Contains(","+parts[1]+",", ",all,")
+		}
+	}
+	numField := reflectType.NumField()
+	for i := 0; i < numField; i++ {
+		fieldValue := reflectValue.Field(i)
+		fieldType := reflectType.Field(i)
+		fieldName := strings.SplitN(fieldType.Tag.Get(s.Tag), ",", 2)[0]
+		if len(fieldName) < 1 || fieldName == "-" {
+			continue
+		}
+		if _, ok := fieldAll[fieldName]; (isExc && ok) || (!isExc && len(fieldAll) > 0 && !ok) {
+			continue
+		}
+		filter := strings.TrimPrefix(fieldType.Tag.Get("filter"), "#")
+		fieldIncNil, fieldIncZero := incNil, incZero
+		if len(filter) > 0 {
+			fieldIncNil = strings.Contains(","+filter+",", ",nil,") || strings.Contains(","+filter+",", ",all,")
+			fieldIncZero = strings.Contains(","+filter+",", ",zero,") || strings.Contains(","+filter+",", ",all,")
+		}
+		if !s.checkValue(fieldValue, fieldIncNil, fieldIncZero) {
+			continue
+		}
+		fieldName = s.NameConv(on, fieldName, fieldType)
+		call(fieldName, fieldAll[fieldName], fieldType, fieldValue.Addr().Interface())
+	}
+}
---
+++ attrscan/attrscan_test.go
@@ -0,0 +1,110 @@
+package attrscan
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/codingeasygo/util/converter"
+)
+
+type IsNilArray []string
+
+type IsZeroArray []string
+
+func (s IsNilArray) IsNil() bool {
+	return s == nil
+}
+
+func (s IsZeroArray) IsZero() bool {
+	return len(s) < 1
+}
+
+type Simple struct {
+	_  string      `json:"-"`
+	A0 int64       `json:"a0"`
+	A1 *int64      `json:"a1"`
+	AX int64       `json:"ax"`
+	B0 string      `json:"b0"`
+	B1 *string     `json:"b1"`
+	BX string      `json:"bx"`
+	C0 float64     `json:"c0"`
+	C1 *float64    `json:"c1"`
+	CX float64     `json:"cx" filter:"#all"`
+	D0 IsNilArray  `json:"d0"`
+	D1 IsZeroArray `json:"d1"`
+	XX string      `json:"-"`
+}
+
+func TestFilterField(t *testing.T) {
+	var fields []string
+	simple := &Simple{
+		A0: 100,
+		A1: converter.Int64Ptr(100),
+		B0: "100",
+		B1: converter.StringPtr("100"),
+		C0: 100,
+		C1: converter.Float64Ptr(100),
+	}
+	{
+		if !Default.checkValue(reflect.ValueOf(nil), true, true) {
+			t.Error("error")
+			return
+		}
+		var intPtr *int
+		if Default.checkValue(reflect.ValueOf(intPtr), false, false) {
+			t.Error("error")
+			return
+		}
+	}
+	{
+		fields = nil
+		FilterFieldCall("test", simple, "", func(fieldName, fieldFunc string, field reflect.StructField, value interface{}) {
+			fields = append(fields, fieldName)
+		})
+		if strings.Join(fields, ",") != "a0,a1,b0,b1,c0,c1,cx" {
+			t.Errorf("%v", fields)
+			return
+		}
+	}
+	{
+		fields = nil
+		FilterFieldCall("test", simple, "#all", func(fieldName, fieldFunc string, field reflect.StructField, value interface{}) {
+			fields = append(fields, fieldName)
+		})
+		if strings.Join(fields, ",") != "a0,a1,ax,b0,b1,bx,c0,c1,cx,d0,d1" {
+			t.Errorf("%v", fields)
+			return
+		}
+	}
+	{
+		fields = nil
+		FilterFieldCall("test", simple, "a0,a1,ax#all", func(fieldName, fieldFunc string, field reflect.StructField, value interface{}) {
+			fields = append(fields, fieldName)
+		})
+		if strings.Join(fields, ",") != "a0,a1,ax" {
+			t.Errorf("%v", fields)
+			return
+		}
+	}
+	{
+		fields = nil
+		FilterFieldCall("test", simple, "^a0,a1,ax#all", func(fieldName, fieldFunc string, field reflect.StructField, value interface{}) {
+			fields = append(fields, fieldName)
+		})
+		if strings.Join(fields, ",") != "b0,b1,bx,c0,c1,cx,d0,d1" {
+			t.Errorf("%v", fields)
+			return
+		}
+	}
+	{
+		fields = nil
+		FilterFieldCall("test", simple, "count(a0),count(a1),ax#all", func(fieldName, fieldFunc string, field reflect.StructField, value interface{}) {
+			fields = append(fields, fieldName)
+		})
+		if strings.Join(fields, ",") != "a0,a1,ax" {
+			t.Errorf("%v", fields)
+			return
+		}
+	}
+}
