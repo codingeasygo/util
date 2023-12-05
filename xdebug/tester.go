5c096beae14d85cfc050e81f27ded7fdf8afa43b
centny <centny@gmail.com>
2023-12-05 11:07:19 +0800

    add case message.

--- xdebug/tester.go
+++ xdebug/tester.go
@@ -1,16 +1,23 @@
 package xdebug
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type CaseTester map[int]int
 
-func (c CaseTester) Run() bool {
+func (c CaseTester) Run(message ...string) bool {
 	c[-1]++
 	ok := c[c[-1]] > 0 || c[0] > 0
+	m := ""
+	if len(message) > 0 {
+		m = fmt.Sprintf("(%v)", strings.Join(message, " "))
+	}
 	if ok {
-		fmt.Printf("\n\n\n>>Case %v is starting\n", c[-1])
+		fmt.Printf("\n\n\n>>Case%v %v is starting\n", m, c[-1])
 	} else {
-		fmt.Printf("\n\n\n>>Case %v is skipped\n", c[-1])
+		fmt.Printf("\n\n\n>>Case%v %v is skipped\n", m, c[-1])
 	}
 	return ok
 }
